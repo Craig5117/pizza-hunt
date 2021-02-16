58f0613af966c37c84c81d372f2614cb76814ed1
Craig5117 <craig.bennett5117@gmail.com>
2021-02-16 11:38:11 -0500

    added comment model and comment count virtual

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,21 +1,39 @@
 const { Schema, model } = require('mongoose');
 
 const PizzaSchema = new Schema({
-    pizzaName: {
-        type: String
+        pizzaName: {
+            type: String
+        },
+        createdBy: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        size: {
+            type: String,
+            default: 'Large'
+        },
+        toppings: [],
+        comments: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Comment'
+            }
+        ]
     },
-    createdBy: {
-        type: String
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    size: {
-        type: String,
-        default: 'Large'
-    },
-    toppings: []
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
+    }
+);
+
+// get a total count of the comments for a given Pizza
+PizzaSchema.virtual('commentCount').get(function() {
+    return this.comments.length;
 });
 
 const Pizza = model('Pizza', PizzaSchema);
